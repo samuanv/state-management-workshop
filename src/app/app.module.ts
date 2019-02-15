374a7d439e61c29825cc3a5fb7855278910920fc
Samuel Andreo <E95620@hugoboss.com>
2019-02-15 09:45:19 +0000

    DevTools and persistState

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { fakeBackendProvider } from './product-list/product-list.interceptor';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+
 @NgModule({
    declarations: [
       AppComponent,
       ProductListComponent
    ],
    imports: [
+			environment.production ? [] : AkitaNgDevtools.forRoot(),
 			BrowserModule,
 			HttpClientModule,
 			CommonModule,
